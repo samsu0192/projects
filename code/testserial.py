0bef2d64c4d8e5528354160403d09ee8bd77c6a7
gitpi <gitpi@gimail.com>
2017-07-12 13:17:09 +0800

    after lunch

--- code/testserial.py
+++ code/testserial.py
@@ -22,11 +22,23 @@ class DI():
         status.append(0)
         counter.append(0)
         prestatus.append(0)
-	def i_write(self):
-		pass
+    def read(self):
+        self.status_all=mymodbus.read_coils(0,count=totalport,unit=1)
+        for i in range(0,totalport):
+            self.status[i]=self.status_all.bits[i]
 di=DI()
 
 #run(host='localhost',port=8080,debug=True)
 
 while(1):
-	di.i_write()
+    di.read()
+	for i in range(0,totalport):
+		if di.status[i]==True:
+			if di.status[i]!=di.prestatus[i]:
+				di.counter[i]+=1
+				for i in monitor_port:
+					counter_all+='port'+str(i)+':'+str(di.counter[i])+' '
+				print 'LED%d is on,Total: '+counter_all
+	for i in range(0,totalport):
+		di.prestatus[i]=di.status[i]
+

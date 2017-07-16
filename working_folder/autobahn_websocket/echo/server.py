7352aa29c973558a4f0b096fc8d2aa50a335d535
samsu <samsu0192@gmail.com>
2017-07-17 01:15:12 +0800

    evening sav e

--- working_folder/autobahn_websocket/echo/server.py
+++ working_folder/autobahn_websocket/echo/server.py
@@ -12,4 +12,25 @@ class MyServerProtocol(WebSocketServerProtocol):
 		print('WebSocket connection open.')
 	
 	def onMessage(self,payload,isBinary):
+		if isBinary:
+			print('Binary message received: {0} bytes'.format(len(payload)))
+		else:
+			print ('Text message received: {0}'.format(payload.decode('utf8')))
+		self.sendMessage(u'Message received')
+	
+	def onClose(self,wasClean,code,reason):
+		
+if __name__='__main__':
+
+	import sys
+	from twisted.python import log
+	from twisted.internet import reactor
+
+	log.startLogging(sys.stdout)
+	factory=WebSocketServerFactory(u'ws://127.0.0.1:9000')
+	factory.protocol=MyServerProtocol
+#factory.setProtocolOptions(maxConnections=2)
+
+	reactor.listenTCP(9000,factory)
+	reactor.run()
 
--- working_folder/autobahn_websocket/example/echo/sample_client.py
+++ working_folder/autobahn_websocket/example/echo/sample_client.py
@@ -1,15 +1,25 @@
 from autobahn.twisted.websocket import WebSocketClientProtocol,\
 		WebSocketClientFactory
 
-class MyClientProtocol(WebSocketClientProtocl):
+class MyClientProtocol(WebSocketClientProtocol):
 	def onConnect(self,response):
 		print('Server connected: {0}'.format(response.peer))
-
+	
 	def onOpen(self):
 		print('WebSocket connection open.')
+	
+		def hello():
+			self.sendMessage(u'Hello,world!'.encode('utf8'))
+			self.sendMessage(b'\x00\x01\x03\x04',isBinary=True)
+			self.factory.reactor.callLater(1,hello)
+
+		#start sending messages every second ..
+		hello()
+	
 	def onMessage(self,payload,isBinary):
 		if isBinary:
 			print('Binary message received: {0} bytes'.format(len(payload)))
+
 		else:
 			print('Text message received: {0}'.format(payload.decode('utf8')))
 	
@@ -17,7 +27,7 @@ class MyClientProtocol(WebSocketClientProtocl):
 		print('WebSocket connection closed: {0}'.format(reason))
 
 if __name__=='__main__':
-
+	
 	import sys
 	from twisted.python import log
 	from twisted.internet import reactor
